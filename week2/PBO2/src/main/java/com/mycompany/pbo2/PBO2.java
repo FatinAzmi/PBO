1ea1e7ac4e07c49ca7a9d3ac00f284df3915cdce
Fatina <fatin@email.com>
2024-08-27 13:29:39 +0700

    Commit Tugas

---
+++ week2/PBO2/src/main/java/com/mycompany/pbo2/PBO2.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pbo2;
+
+/**
+ *
+ * @author MSI THIN
+ */
+public class PBO2 {
+
+    public static void main(String[] args) {
+        System.out.println("Fatina Sabitul Azmi");
+        System.out.println("2340506076");
+        System.out.println("Grabag, Magelang");
+        System.out.println("08990758676");
+    }
+}
